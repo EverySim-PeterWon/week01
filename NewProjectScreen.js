fff12ed002061cf96b2d2250da139e3ac3ccf026
EverySim-PeterWon <peterwon@everysim.io>
2024-12-06 11:47:16 +0900

    Add files via upload

---
+++ NewProjectScreen.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { SelectButton } from "./ProjectButton";
+import { WorkbenchScreen } from "./WorkbenchScreen";
+import { ProjectIdAuto, SetNewProjectData } from "./LocalStorageManage";
+
+// Page of New Project
+export function NewProjectScreen({ onBack }) {
+  // Define useState
+  const [projectId] = React.useState(() => ProjectIdAuto());
+  const [projectName, setProjectName] = React.useState("");
+  const [showWorkbench, setMakeWorkbench] = React.useState(false);
+  const [showNewProject, setNewProject] = React.useState(true);
+
+  // Define handle function of input at project name
+  const handleNewInputChange = (event) => {
+    setProjectName(event.target.value);
+    // console.log(event.target.value);
+  };
+
+  // Define handle function of MAKE button when click
+  const handleButtonMakeClick = () => {
+    localStorage.setItem("project_id", JSON.stringify(projectId));
+    localStorage.setItem("project_name", JSON.stringify(projectName));
+    SetNewProjectData(projectId, projectName);
+    setMakeWorkbench(true);
+    setNewProject(false);
+  };
+
+  return (
+    <div>
+      {!showWorkbench && showNewProject && (
+        <div>
+          <button onClick={onBack}>BACK</button>
+          <h1>NEW PROJECT</h1>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <label htmlFor="inputid">PROJECT ID</label>
+            <input id="inputid" type="number" value={projectId} disabled />
+          </div>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <label htmlFor="inputProjName">PROJECT NAME</label>
+            <input
+              id="inputProjName"
+              placeholder="Input project name"
+              type="text"
+              value={projectName}
+              onChange={handleNewInputChange}
+            />
+          </div>
+          <SelectButton
+            onClick={handleButtonMakeClick}
+            buttonName={"MAKE NEW PROJECT"}
+          />
+        </div>
+      )}
+      {showWorkbench && <WorkbenchScreen />}
+    </div>
+  );
+}
